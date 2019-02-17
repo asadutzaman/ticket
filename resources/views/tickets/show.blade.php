59c4f4ce90db3916fde26a62bbd3980d0359fda1
asadujjaman <asadujjamanrajib@yahoo.com>
2019-02-17 19:58:11 +0000

    ticket view updated

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -25,19 +25,14 @@
             <div class="col-md-6">
                 <div class="box box-success">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Details view</h3>
+                        <div class="text-left"><h3 class="box-title">{{ $ticket->complain_sub }}</h3><br>{{ $ticket->c_name }}<br>{{ $ticket->phone }}</div>
+                        <div class="text-right">Created- {{ $ticket->created_at }}</div>
                     </div>
                     <div class="box-body">
                         <div class="col-xs-12 col-sm-12 col-md-12">
                             <div class="form-group">
-                                <strong>ID:</strong>
+                                <strong>Ticket ID:</strong>
                                 {{ $ticket->id }}
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-12 col-md-12">
-                            <div class="form-group">
-                                <strong>Subject:</strong>
-                                {{ $ticket->complain_sub }}
                             </div>
                         </div>
                         <div class="col-xs-12 col-sm-12 col-md-12">
